b2a741d31bfed5784762497c97a0b658fd43a499
Programador Lorenzo <programadorlorenzo@icloud.com>
2025-08-23 10:29:22 -0500

    fix: Update title of 'pagos' tab from 'Gastos' to 'Pagos'

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -32,7 +32,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="pagos"
         options={{
-          title: 'Gastos',
+          title: 'Pagos',
         }}
       />
       <Tabs.Screen
